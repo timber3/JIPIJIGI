3a894b56b2646d6ff8b3f3c59c7fd096b3f5a4f0
timber3 <timber3@naver.com>
2023-12-20 22:37:12 +0900

    [fix] 파일명 변경 MainController -> BoardController

--- BackEnd/src/main/java/com/jpjg/Ji_Pi_Ji_Gi/board/Controller/BoardController.java
+++ BackEnd/src/main/java/com/jpjg/Ji_Pi_Ji_Gi/board/Controller/BoardController.java
@@ -1,2 +1,14 @@
-package com.jpjg.Ji_Pi_Ji_Gi.board.Controller;public class BoardController {
+package com.jpjg.Ji_Pi_Ji_Gi.board.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/board")
+public class BoardController {
+    
 }
